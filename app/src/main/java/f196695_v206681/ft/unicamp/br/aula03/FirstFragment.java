e5ce7db239929668062d4fec545a960e64d81b9a
Felipe Marchi <fel.marchi@gmail.com>
2019-09-03 18:45:16 -0300

    Aula 03

---
+++ app/src/main/java/f196695_v206681/ft/unicamp/br/aula03/FirstFragment.java
@@ -0,0 +1,30 @@
+package f196695_v206681.ft.unicamp.br.aula03;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+    View view;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (view == null)
+            view = inflater.inflate(R.layout.fragment_first, container, false);
+        return view;
+    }
+
+}
---
+++ app/src/main/java/f196695_v206681/ft/unicamp/br/aula03/MailFragment.java
@@ -0,0 +1,52 @@
+package f196695_v206681.ft.unicamp.br.aula03;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MailFragment extends Fragment {
+    View view;
+    EditText destinatario;
+    EditText mensagem;
+
+    public MailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (view == null)
+            view = inflater.inflate(R.layout.fragment_mail, container, false);
+
+        destinatario = view.findViewById(R.id.destinatario);
+        mensagem = view.findViewById(R.id.mensagem);
+
+
+        Button button = view.findViewById(R.id.btn_enviar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mensagem = "Enviado para: ";
+                mensagem += MailFragment.this.destinatario.getText().toString() + "\n";
+                mensagem += "Mensagem: " + MailFragment.this.mensagem.getText().toString();
+                ((MainActivity) getActivity()).abrirAutoresFragment(mensagem);
+            }
+        });
+        return view;
+    }
+
+}
